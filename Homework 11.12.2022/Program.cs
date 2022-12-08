e964bf6a2b702fc09c9f98486d0dd41338375aa6
Alakbar Hasanov <alakbarbh@code.edu.az>
2022-12-08 08:53:21 +0400

    Homework 11.12.2022

---
+++ Homework 11.12.2022/Program.cs
@@ -0,0 +1,94 @@
+﻿//task1: -Verilmis n ededinin 3-e ve 7-e bolunub, bolunmemesini tapin
+
+//int n = 21;
+
+//if (n % 3 == 0 && nameof % 7 == 0)
+//{
+
+//    Console.Writline("bolunur");
+//}
+
+//else
+//{
+//    Comsole.Writline("bolunmur")
+//}
+
+
+
+//task2: 2-n ve m ededleri verilir.Eger n ve m ededleri her ikisi cutdurse n ve m ededlerinin cemini hesablayin
+
+//int n = 10
+//int m = 15
+//int sum = 0
+
+//if (n % 2 == 0 && m % 2 == 0)
+//{
+//   sum = n + m
+
+//  Console.Writline(sum)
+//}
+
+//else
+//{
+//  Console.Writline("duzgun reqem daxil edin")
+//}
+
+
+
+
+//task3- Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+//int = 5
+//int m = 10
+//int sum = 0
+
+//for (int i = n; i< m; i++)
+//{
+//  if ( i % 2 == 1)
+//  {
+//     sum += i
+//  }
+
+//Console.Writline(sum)
+
+
+
+
+//task4- Verilmis array-in icindeki tek ededlerin cemini tapin
+
+//int[] numbers= {1,3,4,5,9,11,14,20}
+//int sum = 0;
+
+//for (int i = 0; i<numbers.length; i++)
+
+//{
+//   if (numbers[i] %  2 == 1)
+//   {
+//     sum += numbers[i]
+//   }
+
+//}
+//Console.Writline(sum);
+
+
+
+//task5- Verilmis array-in icindeki cut ededlerin cemini tapin
+
+//int[] nums = {1,2,3,4,6,8,11,12,16,19,22}
+//int count = 0;
+
+//foreach (var item in nums)
+//{
+//    if (item % 2 == 0)
+//       count++;
+
+
+//}
+
+//Console.Writline(count);
+
+
+
+
+
+
